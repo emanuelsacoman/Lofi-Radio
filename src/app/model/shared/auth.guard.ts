5c4b651484f0eb15f26928aa72c8b9dea0c5a123
EvS444 <emanuelsacoman@gmail.com>
2025-04-19 11:19:10 -0300

    Mensagem de acesso negado

--- src/app/model/shared/auth.guard.ts
+++ src/app/model/shared/auth.guard.ts
@@ -6,19 +6,31 @@ import {
   UrlTree,
 } from '@angular/router';
 import { Observable } from 'rxjs';
+import { NgToastService } from 'ng-angular-popup';
 import { AuthService } from 'src/app/services/auth.service';
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard {
-  constructor(public authService: AuthService, public router: Router) {}
+  constructor(
+    public authService: AuthService,
+    public router: Router,
+    private toast: NgToastService 
+  ) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | UrlTree | boolean {
     if (this.authService.isLoggedIn !== true) {
-      window.alert('Acesso negado!');
+      this.toast.error({
+        detail: "Erro!",
+        summary: "Acesso negado!",
+        duration: 5000
+      });
       this.router.navigate(['']);
+      return false;
     }
     return true;
   }
