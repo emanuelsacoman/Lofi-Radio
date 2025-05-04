6d74d45266d1f1b92019854a330c1926c09d60f7
EvS444 <emanuelsacoman@gmail.com>
2025-05-04 12:45:26 -0300

    wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { HomeComponent } from './view/website/home/home.component';
 import { LoginComponent } from './view/website/login/login.component';
 import { AdmComponent } from './view/adm/adm/adm.component';
 import { AuthGuard } from './model/shared/auth.guard';
-import { VhsComponent } from './view/effect/vhs/vhs.component';
 
 const routes: Routes = [
   {
